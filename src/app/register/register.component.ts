53f7357603b73fae18bc47f2e0f065267ddcdd2c
elicost4 <eliabecostaa@outlook.com>
2021-05-31 18:25:35 -0300

    add user register and user login

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
+  confirmPassword: string
+  typeUser: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  confirmSenha(event: any ) {
+    this.confirmPassword = event.target.value
+  }
+
+  tipoUser(event: any) {
+    this.typeUser = event.target.value
+  }
+
+  cadastrar() {
+    this.user.tipo = this.typeUser
+
+      if(this.user.senha != this.confirmPassword) {
+        alert('As senhas são diferentes.')
+      } else {
+        this.authService.cadastrar(this.user).subscribe((resp: User) =>{
+          this.user = resp
+          this.router.navigate(['/login'])
+          alert('Usuario cadastrado com sucesso!')
+        })
+      }
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  token: '',
+  nome: '',
+  id: 0,
+  foto: '',
+  tipo: '',
+  server: 'https://blogpessoalelicost4.herokuapp.com'
 };
