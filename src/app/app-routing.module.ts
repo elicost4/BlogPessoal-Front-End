5adbc59938a6ab32aa6f4fd6141d57b10e055449
elicost4 <eliabecostaa@outlook.com>
2021-06-01 19:41:48 -0300

    componente tema sem consumo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
   
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path:'login', component:  LoginComponent},
   {path:'register', component: RegisterComponent},
 
-  {path:'inicio', component: InicioComponent}
+  {path:'inicio', component: InicioComponent},
+  {path: 'tema', component: TemaComponent}
 ];
 
 @NgModule({
